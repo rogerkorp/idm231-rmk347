5aa9eafeefc35df5c333597730885c6e748db008
rogerkorp <rmk347@drexel.edu>
2021-03-19 15:22:21 -0400

    :rocket: Last Minute Fixes and Finalization
    Made a few small changes and deployed my project to the public!

--- javascript/script.js
+++ javascript/script.js
@@ -12,18 +12,18 @@ let displayDate = document.getElementById('dateRange'); //changes the date range
 let info = document.getElementById('info'); // Changes the paragraph about the zodiac
 
 //reads button clicks
-let coffee = document.getElementById("coffee");
-let orange = document.getElementById("orange");
-let milk = document.getElementById("milk");
-let strawberry = document.getElementById("straw");
-let apple = document.getElementById("apple");
-let cereal = document.getElementById("cereal");
-let pancake = document.getElementById("pancake");
-let toast = document.getElementById("toast");
-let bagel = document.getElementById("bagel");
-let donut = document.getElementById("donut");
-let muffin = document.getElementById("muffin");
-let yolk = document.getElementById("yolk");
+let coffee = document.getElementById('coffee');
+let orange = document.getElementById('orange');
+let milk = document.getElementById('milk');
+let strawberry = document.getElementById('straw');
+let apple = document.getElementById('apple');
+let cereal = document.getElementById('cereal');
+let pancake = document.getElementById('pancake');
+let toast = document.getElementById('toast');
+let bagel = document.getElementById('bagel');
+let donut = document.getElementById('donut');
+let muffin = document.getElementById('muffin');
+let yolk = document.getElementById('yolk');
 
 //reads the sound files
 const coffeeSound = document.getElementById('coffeeMP3');
@@ -89,7 +89,7 @@ function showHelp(){ //This functions is responsible for hiding and unhiding the
 
 //uses submitted date information, and converts it into something the algorithm can use.
 function getDate(){ 
-    myDate = new Date (document.getElementById("zodiac-form").value);
+    myDate = new Date (document.getElementById('zodiac-form').value);
     zodiacDay = myDate.getDate() + 1;
     zodiacMonth = myDate.getMonth() + 1;
     getZodiac(zodiacDay, zodiacMonth)
@@ -180,107 +180,109 @@ yolk.addEventListener('click', function(){
 function changeZodiac(zodiac){
   
   if (zodiac === 'coffee'){
-    image.src = "../images/coffee.png"
-    subtitle.innerHTML = "Season of the Coffee";
-    displayDate.innerHTML = "January 20th - February 18th";
-    info.innerHTML = "A warm beverage that wakes you up. Coffee symbolizes energy, a trait common among hardworking achievers. It fills you up with vim and vigor, allowing you to accomplish great things at a rapid pace. Don’t drink too much though, otherwise you’ll end up with the jitters!";
+    image.src = '../images/coffee.png'
+    subtitle.innerHTML = 'Season of the Coffee';
+    displayDate.innerHTML = 'January 20th - February 18th';
+    info.innerHTML = 'A warm beverage that wakes you up. Coffee symbolizes energy, a trait common among hardworking achievers. It fills you up with vim and vigor, allowing you to accomplish great things at a rapid pace. Don’t drink too much though, otherwise you’ll end up with the jitters!';
     noSoundPlease();
     coffeeSound.play();
   }  
   
   if (zodiac === 'orange'){
-    image.src = "../images/orange-juice.png";
-    subtitle.innerHTML = "Season of the Orange Juice";
-    displayDate.innerHTML = "February 19th - March 20th";
-    info.innerHTML = "A cool drink that keeps you strong and healthy. Orange Juice symbolizes bravery, a trait common among the most courageous of leaders. It helps keep your body fit and gives you the strength you need in order to take on any challenge. It also pairs well with any breakfast food!";
+    image.src = '../images/orange-juice.png';
+    subtitle.innerHTML = 'Season of the Orange Juice';
+    displayDate.innerHTML = 'February 19th - March 20th';
+    info.innerHTML = 'A cool drink that keeps you strong and healthy. Orange Juice symbolizes bravery, a trait common among the most courageous of leaders. It helps keep your body fit and gives you the strength you need in order to take on any challenge. It also pairs well with any breakfast food!';
     noSoundPlease();
     orangeSound.play();
   }  
  
   if (zodiac === 'milk'){
-    image.src = "../images/milk.png";
-    subtitle.innerHTML = "Season of the Milk";
-    displayDate.innerHTML = "March 21st - April 19th";
-    info.innerHTML = "A smooth drink that calms your nerves. Milk symbolizes tranquility, a trait common among the serene and harmonious. It has a creamy taste that soothes the soul, allowing you to clear your mind and relax for a while. When heated, Milk could also be used to help you sleep at night.";
+    image.src = '../images/milk.png';
+    subtitle.innerHTML = 'Season of the Milk';
+    displayDate.innerHTML = 'March 21st - April 19th';
+    info.innerHTML = 'A smooth drink that calms your nerves. Milk symbolizes tranquility, a trait common among the serene and harmonious. It has a creamy taste that soothes the soul, allowing you to clear your mind and relax for a while. When heated, Milk could also be used to help you sleep at night.';
     noSoundPlease();
     milkSound.play();
   } 
  
   if (zodiac === 'strawberry'){
-    image.src = "../images/strawberry.png";
-    subtitle.innerHTML = "Season of the Strawberry";
-    displayDate.innerHTML = "April 20th - May 20th";
-    info.innerHTML = "A popular fruit that pairs well with other foods. Strawberries symbolize charisma, the trait of socialization. Their signature red color helps them stick out in the crowd, and their unique flavor is a welcome presence in any meal, whether it’s being mixed in a smoothie, or dolloped in whipped cream for a sweet treat!";
+    image.src = '../images/strawberry.png';
+    subtitle.innerHTML = 'Season of the Strawberry';
+    displayDate.innerHTML = 'April 20th - May 20th';
+    info.innerHTML = 'A popular fruit that pairs well with other foods. Strawberries symbolize charisma, the trait of socialization. Their signature red color helps them stick out in the crowd, and their unique flavor is a welcome presence in any meal, whether it’s being mixed in a smoothie, or dolloped in whipped cream for a sweet treat!';
     noSoundPlease();
     strawberrySound.play();
   }
   
   if (zodiac === 'apple'){
-    image.src = "../images/apple-pie.png";
-    subtitle.innerHTML = "Season of the Apple Pie";
-    displayDate.innerHTML = "May 21st - June 21st";
-    info.innerHTML = "A warm dessert that’s filled with sweet apples and surrounded by a flakey crust. Apple Pie symbolizes mysteries and secrets. The sweet interior of the pie is hidden away by its exterior, preventing anyone from knowing what’s really inside. Who knows what actually lies beneath the surface? For all we know, it might not even have apples in there! ";
+    image.src = '../images/apple-pie.png';
+    subtitle.innerHTML = 'Season of the Apple Pie';
+    displayDate.innerHTML = 'May 21st - June 21st';
+    info.innerHTML = 'A warm dessert that’s filled with sweet apples and surrounded by a flakey crust. Apple Pie symbolizes mysteries and secrets. The sweet interior of the pie is hidden away by its exterior, preventing anyone from knowing what’s really inside. Who knows what actually lies beneath the surface? For all we know, it might not even have apples in there! ';
     noSoundPlease();
     appleSound.play();
   }
   
   if (zodiac === 'cereal'){
-    image.src = "../images/cereal.png";
-    subtitle.innerHTML = "Season of the Cereal";
-    displayDate.innerHTML = "June 22nd - July 22nd";
-    info.innerHTML = "A quickly made meal, usually served in milk. Cereal symbolizes acceptance, a trait common among the most understanding of people. When plans don’t quite work out, this meal is always there for you, ready to be made at a moments notice. While it may not be as lavish as you’d hope, it ends up being your only option, and sometimes you just have to accept that instead of letting it weigh you down.";
+    image.src = '../images/cereal.png';
+    subtitle.innerHTML = 'Season of the Cereal';
+    displayDate.innerHTML = 'June 22nd - July 22nd';
+    info.innerHTML = 'A quickly made meal, usually served in milk. Cereal symbolizes reliability and dependability. When plans don’t quite work out, this meal is always there for you, ready to be made at a moment’s notice. By aligning with this sign, you will find yourself greatly valued by others, and you will form many loyal friendships.';
     noSoundPlease();
     cerealSound.play();
   }
   
   if (zodiac === 'pancake'){
-    image.src = "../images/pancakes.png";
-    subtitle.innerHTML = "Season of the Pancake";
-    displayDate.innerHTML = "July 23nd - August 22nd";
-    info.innerHTML = "A stack of flapjacks coated in butter and drenched in maple syrup. Pancakes symbolize order, the trait of calculated strategists. While initially made of a formless batter, pancakes bake into round disks that can then be built on top of each other, thereby forming towering meals. By taking the chaos and applying rules and structure to it you are capable of accomplishing great things.";
+    image.src = '../images/pancakes.png';
+    subtitle.innerHTML = 'Season of the Pancake';
+    displayDate.innerHTML = 'July 23nd - August 22nd';
+    info.innerHTML = 'A stack of flapjacks coated in butter and maple syrup. Pancakes symbolize balance, a trait valued by those who seek the most well-rounded lifestyles. While problems may keep mounting on top of each other, your ability to strategize and think clearly will prevent everything from falling apart.';
     noSoundPlease();
     pancakeSound.play();
   }
   
   if (zodiac === 'toast'){
-    image.src = "../images/toast.png";
-    subtitle.innerHTML = "Season of the Toast";
-    displayDate.innerHTML = "August 23rd - September 22nd";
-    info.innerHTML = "A piece of bread that’s been toasted. Toast symbolizes confidence, a trait common among those with the greatest sense of self. Its presence is immediately known through the pop of a toaster, and whether it’s topped with butter, jam, eggs, or any other food, at the end of the day it’s still just toast.";
+    image.src = '../images/toast.png';
+    subtitle.innerHTML = 'Season of the Toast';
+    displayDate.innerHTML = 'August 23rd - September 22nd';
+    info.innerHTML = 'A piece of bread that’s been toasted. Toast symbolizes identity, and the confidence to know oneself. Despite it appearing in many forms, and whether it’s topped with butter, jam, eggs, at the end of the day it’s still knows that it’s toast. People aligned with this sign tend to have a deep understanding how who they are and are willing to accept changes in life instead of running away from them.';
     noSoundPlease();
     toastSound.play();
   }
   
   if (zodiac === 'bagel'){
-    image.src = "../images/bagel.png";
-    subtitle.innerHTML = "Season of the Bagel";
-    displayDate.innerHTML = "September 23rd - October 23rd";
-    info.innerHTML = "A form of bread in the shape of a ring. Bagels symbolize well-roundedness, a trait common among those with the most balanced lifestyles. While a bagel is missing its center piece, it still has everything else aside from that, making it more a fulfilling meal than that one piece alone could provide. By trying out many things and learning many skills, you could end up living a far more enriching life than if you had focused on only one.";
+    image.src = '../images/bagel.png';
+    subtitle.innerHTML = 'Season of the Bagel';
+    displayDate.innerHTML = 'September 23rd - October 23rd';
+    info.innerHTML = 'A form of bread in the shape of a ring. Bagels represent order and structure. You value having control in your life and are always ready to form a plan of action before doing anything. While you may seem boring at first, your ability to collaborate and work well with others allows you to enhance anyone’s talents.';
     noSoundPlease();
     bagelSound.play();
   }
   
   if (zodiac === 'donut'){
-    image.src = "../images/donut.png";
-    subtitle.innerHTML = "Season of the Donut";
-    displayDate.innerHTML = "October 24th - November 21st";
-    info.innerHTML = "A small, cake-like dessert in the shape of a ring. Donuts symbolize fun, a trait common among the creative and the wild. They can come in many different forms, varying in color, flavor, frosting, and even sprinkles. This allows them to have a near infinite number of combinations, and while they may not necessarily be the best thing to have in the morning, they always make for an enjoyable treat. ";
+    image.src = '../images/donut.png';
+    subtitle.innerHTML = 'Season of the Donut';
+    displayDate.innerHTML = 'October 24th - November 21st';
+    info.innerHTML = 'A small, cake-like dessert in the shape of a ring. Donuts symbolize creativity and fun. You value having new experiences and more variety in the things you do, and you tend reject normalcy in favor of a more chaotic lifestyle. Your always on the lookout for the next big thing, and you’re usually the first to embrace innovation. Donuts also go great with coffee.';
     noSoundPlease();
     donutSound.play();
   }
   
   if (zodiac === 'muffin'){
-    image.src = "../images/muffin.png";
-    subtitle.innerHTML = "Season of the Muffin";
-    info.innerHTML = "A tiny cake that’s baked in an oven. Muffins symbolize wisdom. Similar to how our experiences in the world shape us over time, the muffin starts off small but as it incubates in the oven it will eventually develop into something that’s far more enriching than before. They also taste really good with coffee.";
+    image.src = '../images/muffin.png';
+    subtitle.innerHTML = 'Season of the Muffin';
+    displayDate.innerHTML = 'November 22nd - December 21st';
+    info.innerHTML = 'A tiny cake that’s baked in an oven. Muffins symbolize knowledge and growth, a trait common among the wise. You are greatly shaped by your experiences, and you have a deeper understanding of the world around you. Just remember to take your time, because otherwise you’ll end up with an underbaked view on the things in your life.';
     noSoundPlease();
     muffinSound.play();
   }
   
   if (zodiac === 'yolk'){
-    image.src = "../images/yolk.png";
-    subtitle.innerHTML = "Season of the Yolk";
-    info.innerHTML = "The interior of an egg. The Yolk symbolizes life, the trait of those who embrace new beginnings. While we cannot change the past, we can always embrace the future and create something new. The Yolk tells us that the future is never certain, and that what it ends up becoming is all up for you to decide.";
+    image.src = '../images/yolk.png';
+    subtitle.innerHTML = 'Season of the Yolk';
+    displayDate.innerHTML = 'December 22nd - January 19th';
+    info.innerHTML = 'The yellow interior of an egg. The Yolk symbolizes life and new beginnings. You tend to take personal action and pave your way towards the future instead of passively accepting the things around you. You have a deep understanding of what you can control in life, and you do anything it takes to achieve your goals.';
     noSoundPlease();
     yolkSound.play();
   }
